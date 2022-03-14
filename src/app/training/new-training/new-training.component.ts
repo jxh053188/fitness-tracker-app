b1808402cbdbec9a6e40d48343763dbe1ce56954
Jarred Harkness <jarred.harkness@buhlergroup.com>
2022-03-14 15:34:22 +0100

    add stop and cancel to training service

---
+++ src/app/training/new-training/new-training.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Exercise } from '../exercise.module';
+import { TrainingService } from '../training.service';
+
+@Component({
+  selector: 'app-new-training',
+  templateUrl: './new-training.component.html',
+  styleUrls: ['./new-training.component.css']
+})
+export class NewTrainingComponent implements OnInit {
+  @Output() trainingStart = new EventEmitter<void>();
+
+  exercises: Exercise[];
+
+  constructor(private trainingService: TrainingService) { }
+
+  ngOnInit(): void {
+    this.exercises = this.trainingService.getAvailableExercises()
+  }
+
+  onStartTraining(form: NgForm){
+    this.trainingService.startExercise(form.value.exercise);
+  }
+
+}
